84a3413a5089f55b2543aee0b4f3502276f96057
Prakash kumar <110119311+Prakashkumar526@users.noreply.github.com>
2023-07-25 08:14:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,183 +5,97 @@ let topNews = document.querySelector('.topNews')
 let sportsNews = document.querySelector('#sportsNews .newsBox')
 let businessNews = document.querySelector('#businessNews .newsBox')
 let techNews = document.querySelector('#techNews .newsBox')
-let entertainmentnews = document.querySelector("#entertainmentnews .newsBox")
 
-let header = document.querySelector('.header')
-let toggleMenu = document.querySelector('.bar')
-let menu = document.querySelector('nav ul')
-
+let header = document.querySelector(".header")
+let bar = document.querySelector(".bar")
+let menu = document.querySelector(".nav ul")
 const toggle = (e) => {
-    toggleMenu.classList.toggle('active')
-    menu.classList.toggle('activeMenu')
+  bar.classList.toggle('active')
+  menu.classList.toggle('activemenu')
 }
-
-toggleMenu.addEventListener('click', toggle)
-
-
+bar.addEventListener('click', toggle)
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        header.classList.add('sticky')
-    }
-    else {
-        header.classList.remove('sticky')
-    }
+  if (window.scrollY > 50) {
+    header.classList.add('sticky')
+  }
+  else {
+    header.classList.remove('sticky')
+  }
 })
 
+document.addEventListener('DOMContentLoaded', () => {
+  const apiKey = "8d32cde55879d7c166afbaa8fb1174ed";
 
+  const fetchData = async (category, pageSize) => {
+    const url = `https://gnews.io/api/v4/top-headlines?category=${category}&pageSize=${pageSize}&lang=en&country=IN&apikey=${apiKey}`;
+    const data = await fetch(url);
+    const response = await data.json();
+    return response.articles;
+  };
 
+  const add_breakingNews = (data) => {
+    const breakingImg = document.getElementById('breakingImg');
+    const breakingNews_title = document.querySelector('#breakingNews .title');
+    const breakingNews_desc = document.querySelector('#breakingNews .description');
 
+    breakingImg.innerHTML = `<img src="${data[0].image}" alt="image">`;
+    breakingNews_title.innerHTML = `<a href="${data[0].url}" target="_blank"><h2>${data[0].title}</h2></a>`;
+    breakingNews_desc.innerHTML = `${data[0].description}`;
+  };
 
+  fetchData('general', 5).then(add_breakingNews);
 
+  const add_topNews = (data) => {
+    const topNews = document.querySelector('.topNews');
 
-// fetching news data from a website providing api
-
-const apiKey = "42b019b6c01c46cab0f2a8ee42873594"
-
-const fetchData = async (category, pageSize) => {
-    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&pageSize=${pageSize}&apiKey=${apiKey}`
-    const data = await fetch(url)
-    const response = await data.json()
-    console.log(response);
-    return response.articles
-
-}
-// fetchData('general',5)
-
-//adding breaking news
-
-const add_breakingNews = (data) => {
-    breakingImg.innerHTML = `<img src=${data[0].urlToImage} alt="image">`
-    breakingNews_title.innerHTML = `<a href=${data[0].url} target="_blank"><h2>${data[0].title}</h2></a>`
-    breakingNews_desc.innerHTML = `${data[0].description}`
-}
-fetchData('general', 5).then(add_breakingNews)
-
-const add_topNews = (data) => {
-    let html = ''
-    let title = ''
-    data.forEach((element) => {
-        if (element.title.length < 100) {
-            title = element.title
-        }
-        else {
-            title = element.title.slice(0, 100) + "..."
-        }
-
-        html += `<div class="news">
-                    <div class="img">
-                        <img src=${element.urlToImage} alt="image">
-                    </div>
-                    <div class="text">
-                        <div class="title">
-                        <a href=${element.url} target="_blank"><p>${title}</p></a>
-                        </div>
-                    </div>
-                </div>`
-    })
-    topNews.innerHTML = html
-}
-fetchData('general', 20).then(add_topNews)
-
-const add_sportsNews = (data) => {
-    let html = ''
-    let title = ''
-    data.forEach((element) => {
-        if (element.title.length < 100) {
-            title = element.title
-        }
-        else {
-            title = element.title.slice(0, 100) + "..."
-        }
-
-        html += `<div class="newsCard">
-                    <div class="img">
-                        <img src=${element.urlToImage} alt="image">
-                    </div>
-                    <div class="text">
-                        <div class="title">
-                        <a href=${element.url} target="_blank"><p>${title}</p></a>
-                        </div>
-                    </div>
-                </div>`
-    })
-    sportsNews.innerHTML = html
-}
-fetchData('sports', 5).then(add_sportsNews)
-const add_businessNews = (data) => {
-    let html = ''
-    let title = ''
-    data.forEach((element) => {
-        if (element.title.length < 100) {
-            title = element.title
-        }
-        else {
-            title = element.title.slice(0, 100) + "..."
-        }
-
-        html += `<div class="newsCard">
-                    <div class="img">
-                        <img src=${element.urlToImage} alt="image">
-                    </div>
-                    <div class="text">
-                        <div class="title">
-                        <a href=${element.url} target="_blank"><p>${title}</p></a>
-                        </div>
-                    </div>
-                </div>`
-    })
-    businessNews.innerHTML = html
-}
-fetchData('business', 5).then(add_businessNews)
-const add_techNews = (data) => {
-    let html = ''
-    let title = ''
-    data.forEach((element) => {
-        if (element.title.length < 100) {
-            title = element.title
-        }
-        else {
-            title = element.title.slice(0, 100) + "..."
-        }
-
-        html += `<div class="newsCard">
-                    <div class="img">
-                        <img src=${element.urlToImage} alt="image">
-                    </div>
-                    <div class="text">
-                        <div class="title">
-                        <a href=${element.url} target="_blank"><p>${title}</p></a>
-                        </div>
-                    </div>
-                </div>`
-    })
-    techNews.innerHTML = html
-}
-fetchData('technology', 5).then(add_techNews)
-
-
-const add_entertainmentnews = (data) => { // Corrected function name
-    let html = ``
-    let title = ''
-    data.forEach((element) => {
-        if (element.title.length < 100) {
-            title = element.title;
-        } else {
-            title = element.title.slice(0, 100) + "...";
-        }
-        html += `<div class="newsCard">
-        <div class="img">
-            <img src=${element.urlToImage} alt="image">
-        </div>
-        <div class="text">
-            <div class="title">
-            <a href=${element.url} target="_blank"><p>${title}</p></a>
+    let html = '';
+    data.slice(0, 5).forEach((element) => {
+      const title = element.title.length < 100 ? element.title : element.title.slice(0, 100) + "...";
+      html += `
+          <div class="news">
+            <div class="img">
+              <img src="${element.image}" alt="image">
             </div>
-        </div>
-    </div>`
-    })
-    entertainmentnews.innerHTML = html; // Corrected ID selector
-}
+            <div class="text">
+              <div class="title">
+                <a href="${element.url}" target="_blank"><p>${title}</p></a>
+              </div>
+            </div>
+          </div>
+        `;
+    });
+    topNews.innerHTML = html;
+  };
 
-fetchData('entertainment', 5).then(add_entertainmentnews) // Corrected function call
+  fetchData('general', 20).then((data) => add_topNews(data.slice(0, 5)));
+
+  const add_news = (category, containerId) => {
+    fetchData(category, 5).then((data) => {
+      const newsBox = document.querySelector(`#${containerId} .newsBox`);
+
+      let html = '';
+      data.forEach((element) => {
+        const title = element.title.length < 100 ? element.title : element.title.slice(0, 100) + "...";
+        html += `
+            <div class="newsCard">
+              <div class="img">
+                <img src="${element.image}" alt="image">
+              </div>
+              <div class="text">
+                <div class="title">
+                  <a href="${element.url}" target="_blank"><p>${title}</p></a>
+                </div>
+              </div>
+            </div>
+          `;
+      });
+      newsBox.innerHTML = html;
+    });
+  };
+
+  add_news('sports', 'sportsNews');
+  add_news('business', 'businessNews');
+  add_news('technology', 'techNews');
+  add_news('entertainment', 'entertainmentnews');
+});
